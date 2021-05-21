3a7c560ca30635516c922407c949b7b455c4cea7
wesley7104 <wesley7104@gmail.com>
2021-05-20 21:36:51 -0500

    Corrected Adapter fro Vercel

--- .history/svelte.config_20210516035104.js
+++ .history/svelte.config_20210516035104.js
@@ -3,9 +3,9 @@ import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
+		target: '#svelte'
 	}
 };
 
